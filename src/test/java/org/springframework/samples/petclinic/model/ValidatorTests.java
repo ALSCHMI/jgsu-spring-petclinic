8b39b2b8bf4d5fc742d0df5eaa81f02804a7b1ff
Schmidt <alexander.schmidt@materna.de>
2023-01-22 23:01:10 +0100

    remove test breakage

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -44,8 +44,8 @@ class ValidatorTests {
 	void shouldNotValidateWhenFirstNameEmpty() {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
-		Person person = new Person(); 
-		person.setFirstName("notempty");
+		Person person = new Person();
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
